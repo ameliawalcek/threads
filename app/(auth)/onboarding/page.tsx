1bfe39b1704435cda0b1ed908ef3c51dbf14f7dc
ameliawalcek <ameliawalcek@gmail.com>
2023-08-04 16:22:30 +0100

    reinitialize application, global css, auth route, navigation bars

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function page() {
+  return (
+    <main>
+      <h1 className="head-text">onbarding</h1>
+    </main>
+  );
+}
+
+export default page;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main>
-      <h1>threads</h1>
-    </main>
-  );
+    <>
+      <h1 className="head-text text-left">home</h1>
+    </>
+  )
 }
